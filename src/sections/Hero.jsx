f9d9a172a48fafefc70a8794e90b52a4bba3d06d
Calvin <calvinrodrigues500@gmail.com>
2023-12-13 23:22:52 +0530

    Added hero background

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import Button from "../components/Button"
 import { arrowRight } from "../assets/icons"
 import { statistics } from "../constants"
+import { bigShoe1 } from "../assets/images"
 
 const Hero = () => {
   return (
@@ -29,6 +30,9 @@ const Hero = () => {
             ))}
           </div>
         </div>
+          <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+            <img src={bigShoe1} alt="shoe" className="object-contain relative z-10" width={610} height={500}/>
+          </div>
     </section>
   )
 }
